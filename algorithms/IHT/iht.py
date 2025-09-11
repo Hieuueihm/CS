b15265995351ff1191063f23a130bdcaf1d10094
Hieuueihm <gbmhieu123@gmail.com>
2025-09-11 23:28:44 +0700

    add iht

---
+++ algorithms/IHT/iht.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def cs_iht(y, T_Mat, k= None, itermax=1000, patience=20):
+    """
+    Iterative Hard Thresholding (IHT) for Compressive Sensing
+    y = T_Mat * x,  T_Mat is n-by-m
+
+    Parameters:
+        y       : ndarray, measurements (n,)
+        T_Mat   : ndarray, sensing matrix (n,m)
+        k       : int, sparsity (if None, use len(y)//4)
+        itermax : int, maximum iterations
+        tol     : float, convergence tolerance
+
+    Returns:
+        hat_x   : ndarray, recovered signal (m,)
+    """
+    m = T_Mat.shape[1]
+    hat_x_tp = np.zeros(m)   # initialization
+    T_Mat = T_Mat / np.linalg.norm(T_Mat, axis=0, keepdims=True)
+
+    # L = np.linalg.norm(T_Mat, 2) ** 2
+    # u = 1.0 / L
+    # print(u)
+    u = 0.5
+    last_support = set()
+    stable_count = 0
+    if k is None:
+    	k = len(y) // 4
+    for it in range(itermax):
+
+        x_increase = T_Mat.T @ (y - T_Mat @ hat_x_tp)
+        hat_x = hat_x_tp + u * x_increase
+
+        pos = np.argsort(hat_x)[::-1] # sx giam dan
+        hat_x[pos[k:]] = 0
+        support = set(np.nonzero(hat_x)[0])
+        if support == last_support:
+            stable_count += 1
+        else:
+            stable_count = 0
+        last_support = support
+
+        if stable_count >= patience:
+            break
+        hat_x_tp = hat_x
+
+    return hat_x
---
+++ algorithms/IHT/iht_2d.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io, color
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from scipy.fftpack import dct, idct
+from sklearn.linear_model import OrthogonalMatchingPursuit
+import time
+from iht import cs_iht
+def demo_cs_iht(image_path="lena.bmp"):
+    # Đọc ảnh grayscale
+    img = io.imread(image_path, as_gray=True)
+    img = img * 255.0
+    height, width = img.shape
+
+    # Measurement matrix Phi
+    m = height // 3
+    Phi = np.random.randn(m, height)
+    Phi /= np.linalg.norm(Phi, axis=0, keepdims=True)
+
+    # DCT basis (1D)
+    mat_dct_1d = np.zeros((height, height))
+    for k in range(height):
+        dct_vec = np.cos(np.arange(height) * k * np.pi / height)
+        if k > 0:
+            dct_vec -= np.mean(dct_vec)
+        mat_dct_1d[:, k] = dct_vec / np.linalg.norm(dct_vec)
+
+    # Projection
+    img_cs_1d = Phi @ img
+
+    # Recover using OMP
+    sparse_rec_1d = np.zeros((height, width))
+    Theta_1d = Phi @ mat_dct_1d
+    start = time.time()
+
+    for i in range(width):
+        column_rec = cs_iht(img_cs_1d[:, i], Theta_1d)
+        sparse_rec_1d[:, i] = column_rec.T
+
+
+	# Reconstruct ảnh
+    img_rec_1d = mat_dct_1d @ sparse_rec_1d
+    end = time.time()
+	# PSNR
+    rec_psnr = psnr(img, img_rec_1d, data_range=img.max() - img.min())
+
+	# Hiển thị kết quả
+    plt.figure(figsize=(10, 6))
+    plt.subplot(2, 2, 1), plt.imshow(img, cmap="gray"), plt.title("Original Image"), plt.axis("off")
+    plt.subplot(2, 2, 2), plt.imshow(Phi, cmap="gray"), plt.title("Measurement Matrix"), plt.axis("off")
+    plt.subplot(2, 2, 3), plt.imshow(mat_dct_1d, cmap="gray"), plt.title("1D DCT Basis"), plt.axis("off")
+    plt.subplot(2, 2, 4), plt.imshow(img_rec_1d, cmap="gray"), plt.title(f"Reconstructed ({rec_psnr:.2f} dB)"), plt.axis("off")
+    plt.show()
+    print(f"time {end - start}")
+
+
+#------------------------------
+# Run Demo
+#------------------------------
+demo_cs_iht("../lena.bmp")  
